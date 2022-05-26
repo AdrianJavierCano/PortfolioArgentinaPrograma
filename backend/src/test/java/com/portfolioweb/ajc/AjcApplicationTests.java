8f1c9ea119d126edad1c6339cc816a65b6a3b5c8
Adrian Javier Cano <canoadrianjavier@gmail.com>
2022-05-25 21:41:44 -0300

    tercer commit desde vscode

---
+++ backend/src/test/java/com/portfolioweb/ajc/AjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.ajc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
